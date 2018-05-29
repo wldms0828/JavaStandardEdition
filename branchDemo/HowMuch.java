3899f207f1e849d352a2a49470f59b93ceffa815
wldms0828 <whatsportskim@gmail.com>
2018-05-29 15:59:18 +0900

    branch 추가

--- branchDemo/HowMuch.java
+++ branchDemo/HowMuch.java
@@ -4,23 +4,22 @@ public class HowMuch{
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("얼마에요?");
 		int num1 = scanner.nextInt();
-		System.out.println("100원입니다");
+		System.out.println(num1 + "원입니다");
 		System.out.println("몇개 드릴까요");
 		int num2 = scanner.nextInt();
-		System.out.println("5개 주세요");
-		int num3 = scanner.nextInt();
-		System.out.println("총 금액은 500원 입니다");
+		System.out.println(num2 + "개 주세요");
+		int num3 = num1 * num2;
+		System.out.println("총 금액은" + num3 + "원 입니다");
 		System.out.println("비싸요 깍아주세요");
-		int num4 = scanner.nextInt();
 		System.out.println("몇 퍼센트 DC 할께요?");
+		int percent = scanner.nextInt();
+		int result	= num3 - (num3*percent/100);
 		
-		
-		
-		if( 10 =< num4 ){
-			System.out.println("475원 입니다.");
+		if(  percent>=10 ){
+			System.out.println("안팔아요");
 
 		}else{ 
-			System.out.println("안팔아요");
+			System.out.println(result + "원입니다");
 
 		}
 	}
